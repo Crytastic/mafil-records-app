8041cfdc021d20ca9935b64fc24ee10f322f222f
Maxmilián Šeffer <maxmilian.seffer@roboauto.tech>
2023-04-24 23:17:36 +0200

    Revert "add possible mobile view fix"
    This reverts commit de32d3c51656a54f02f9ca0bf098be83d1db3866.

--- frontend/src/components/ResizableSidebar.tsx
+++ frontend/src/components/ResizableSidebar.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { isMobile } from 'react-device-detect';
 import { useState, useEffect, useRef } from "react";
 import "./ResizableSidebar.css";
 import { Stage } from "./Stage";
@@ -19,7 +18,7 @@ export function ResizableSidebar({ stage, open, toggleDrawer }: ResizableSidebar
   const { sidebarWidth, setSidebarWidth } = React.useContext(SidebarContext);
   const [isResizing, setIsResizing] = useState(false);
   const [maxWidth, setMaxWidth] = useState(window.innerWidth * 0.75);
-  const minWidth = isMobile ? window.innerWidth : 150;
+  const minWidth = 150;
 
   const updateMaxWidth = () => {
     if (open && sidebarWidth > window.innerWidth) {
