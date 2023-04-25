1334e7c30f26d1b3be5188bfdfff097dabf9acd9
Maxmilian Seffer <max.seffer@gmail.com>
2023-04-25 22:28:17 +0000

    working OIDC login (minus supplied client secret)

--- frontend/src/oidc-config.tsx
+++ frontend/src/oidc-config.tsx
@@ -6,10 +6,10 @@ const oidcConfig = {
   client_id: '39a00844-84a6-4459-85fb-91d29539088c',
   redirect_uri: 'https://records.devel.mafildb.ics.muni.cz/oidc-login',
   response_type: 'code',
-  scope: 'openid profile email',
-  post_logout_redirect_uri: 'https://records.devel.mafildb.ics.muni.cz/',
+  scope: 'openid profile email eduperson_entitlement',
+  // post_logout_redirect_uri: 'https://records.devel.mafildb.ics.muni.cz/',
   automaticSilentRenew: false,
-  //silent_redirect_uri: 'http://localhost:3000/silent-renew.html',
+  // silent_redirect_uri: 'http://localhost:3000/silent-renew.html',
 };
 
 export default oidcConfig;
