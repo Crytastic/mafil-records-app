ab7c28ffd014c755297ef726c2955c3bd0e0ad02
Maxmilian Seffer <max.seffer@gmail.com>
2023-05-07 22:37:39 +0000

    add SmallLoadingBox, tmp fix for post logout redirect

--- frontend/src/components/common/LoadingBox.tsx
+++ frontend/src/components/common/LoadingBox.tsx
@@ -6,12 +6,41 @@ interface LoadingBoxProps {
   loadingMessage: string;
 }
 
+export function SmallLoadingBox({ loadingMessage }: LoadingBoxProps) {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "left",
+        alignItems: "flex-start",
+      }}
+    >
+      <Box sx={{ display: "flex", alignItems: "center", }}>
+        <Box>
+          <CircularProgress color="primary" thickness={8} size={35} />
+        </Box>
+        <Box padding={2}>{loadingMessage}</Box>
+      </Box>
+    </Box >
+  );
+}
+
 function LoadingBox({ loadingMessage }: LoadingBoxProps) {
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '70vh' }}>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "70vh",
+      }}
+    >
       <CircularProgress color="primary" thickness={4} size={80} />
-      <Box padding={3}>{loadingMessage}</Box>
-    </Box>);
+      <Box padding={4}>{loadingMessage}</Box>
+    </Box>
+  );
 }
 
 export default LoadingBox;
