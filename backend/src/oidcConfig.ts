1b550b91511b93e46339a9a5e3ecf4b33adb47c0
Maxmilián Šeffer <maxmilian.seffer@roboauto.tech>
2023-05-07 21:06:59 +0200

    rename client_secret env var to REACT_APP_CLIENT_SECRET

--- backend/src/oidcConfig.ts
+++ backend/src/oidcConfig.ts
@@ -1,7 +1,7 @@
 const oidcConfig = {
     authority: 'https://oidc.muni.cz/oidc',
     client_id: '39a00844-84a6-4459-85fb-91d29539088c',
-    client_secret: process.env.CLIENT_SECRET,
+    client_secret: process.env.REACT_APP_CLIENT_SECRET,
     redirect_uri: 'https://records.devel.mafildb.ics.muni.cz/oidc-login',
     response_type: 'code',
     scope: 'openid profile email eduperson_entitlement',
