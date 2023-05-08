3a3759dc055a018c9bf0414e53ee2f2424f96dc0
Maxmilián Šeffer <maxmilian.seffer@roboauto.tech>
2023-05-08 14:39:48 +0200

    add server logs for database testing

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const express = require("express");
 const { Pool } = require('pg');
 const cors = require('cors');
@@ -7,12 +6,20 @@ const port = process.env.PORT || 4000;
 
 app.use(express.json());
 
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
 app.get("/", (req, res) => {
   res.send(`Backend server is running on port ${port}`);
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.get("/api", (req, res) => {
+  res.send(`Backend API accessible`);
+});
+
+app.get('/api/series', async (req, res) => {
+  res.send(`Saving series API accessible`);
 });
 
 const connectionString = process.env.DATABASE_URL;
@@ -25,29 +32,17 @@ app.use(cors());
 app.use(express.json());
 
 app.post('/api/series', async (req, res) => {
-  const { seriesInstanceUID, seriesData } = req.body;
-  try {
-    await pool.query(
-      'INSERT INTO series (series_instance_uid, series_data) VALUES ($1, $2) ON CONFLICT (series_instance_uid) DO UPDATE SET series_data = $2',
-      [seriesInstanceUID, JSON.stringify(seriesData)]
-    );
-    res.status(201).send();
-  } catch (err) {
-    console.error(err);
-    res.status(500).send();
-  }
+  console.log('Saving series data to database:', req.body);
+  res.status(200).send();
 });
 
 app.get('/api/series/:seriesInstanceUID', async (req, res) => {
   const { seriesInstanceUID } = req.params;
   try {
-    const { rows } = await pool.query('SELECT series_data FROM series WHERE series_instance_uid = $1', [seriesInstanceUID]);
+    const { rows } = await pool.query('SELECT series_data FROM seriesdt WHERE series_instance_uid = $1', [seriesInstanceUID]);
     res.status(200).json(rows[0]?.series_data ?? null);
   } catch (err) {
     console.error(err);
     res.status(500).send();
   }
 });
-
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
